c519625567d3e80515accf84df73c698680774c6
fede_lastalitas <ing.finterlandi@gmail.com>
2022-09-06 11:13:23 -0300

    triyng to get all countries and put it on my db. Still fail

--- api/src/db.js
+++ api/src/db.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
+const axios = require("axios").default;
+
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
   {
@@ -41,6 +43,62 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+
+// axios.get('https://restcountries.com/v3.1/name/argentina')
+// .then(country => {
+//   return country.data[0];
+// })
+// .then(async (data) => {
+//   //console.log(data);
+//   await sequelize.models.Country.create({
+//         id: data.cca3,
+//         name: data.name.common,
+//         flagImg: data.flags.png,
+//         continent: data.region,
+//         capital: data.capital[0],
+//         subregion: data.subregion,
+//         area: data.area,
+//         population: data.population,
+//   })
+
+//   console.log({
+//     id: data.cca3,
+//         name: data.name.common,
+//         flagImg: data.flags.png,
+//         continent: data.region,
+//         capital: data.capital[0],
+//         subregion: data.subregion,
+//         area: data.area,
+//         population: data.population
+//   })
+// })
+
+axios.get('https://restcountries.com/v3.1/all')
+.then(countries => {
+  return countries.data
+})
+.then(countries =>{
+  countries.forEach(
+    async country =>{
+    await sequelize.models.Country.create({
+              id: country.cca3,
+              name: country.name.common,
+              flagImg: country.flags.png,
+              continent: country.region,
+              capital: Array.isArray(country.capital) ? country.capital[0] : country.capital ,
+              subregion: country.subregion,
+              area: country.area,
+              population: country.population,
+        })
+  }
+  //country => console.log(country.name.common)
+  )
+  //console.log(countries.filter(country => country.capial === undefined))
+})
+
+
+
+
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
