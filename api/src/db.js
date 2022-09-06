a8e0ef6bed6bd5eb91325ff7b70a5d791edeeffd
FEDEIZ <ing.finterlandi@gmail.com>
2022-09-06 17:34:31 -0300

    getCountries OK

--- api/src/db.js
+++ api/src/db.js
@@ -39,66 +39,33 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-// const { Pokemon } = sequelize.models;
+const { Country, Activity } = sequelize.models;
 
 // Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+Country.belongsToMany(Activity, { through: "country_activity" });
+Activity.belongsToMany(Country, { through: "country_activity" });
 
-// axios.get('https://restcountries.com/v3.1/name/argentina')
-// .then(country => {
-//   return country.data[0];
-// })
-// .then(async (data) => {
-//   //console.log(data);
-//   await sequelize.models.Country.create({
-//         id: data.cca3,
-//         name: data.name.common,
-//         flagImg: data.flags.png,
-//         continent: data.region,
-//         capital: data.capital[0],
-//         subregion: data.subregion,
-//         area: data.area,
-//         population: data.population,
+// axios
+//   .get(`${API_URL}/all`)
+//   .then((countries) => {
+//     return countries.data;
 //   })
-
-//   console.log({
-//     id: data.cca3,
-//         name: data.name.common,
-//         flagImg: data.flags.png,
-//         continent: data.region,
-//         capital: data.capital[0],
-//         subregion: data.subregion,
-//         area: data.area,
-//         population: data.population
+//   .then((countries) => {
+//     countries.forEach(async (c) => {
+//       await Country.upsert({
+//         id: c.cca3,
+//         name: c.name.common,
+//         flagImg: c.flags[0],
+//         continent: c.region,
+//         capital: c.capital ? c.capital[0] : "NONE",
+//         subregion: c.subregion,
+//         area: c.area,
+//         population: c.population,
+//       });
+//     });
 //   })
-// })
-
-axios.get('https://restcountries.com/v3.1/all')
-.then(countries => {
-  return countries.data
-})
-.then(countries =>{
-  countries.forEach(
-    async country =>{
-    await sequelize.models.Country.create({
-              id: country.cca3,
-              name: country.name.common,
-              flagImg: country.flags.png,
-              continent: country.region,
-              capital: Array.isArray(country.capital) ? country.capital[0] : country.capital ,
-              subregion: country.subregion,
-              area: country.area,
-              population: country.population,
-        })
-  }
-  //country => console.log(country.name.common)
-  )
-  //console.log(countries.filter(country => country.capial === undefined))
-})
-
-
-
-
+//   .then(() => console.log("Successfully loaded from api"))
+//   .catch((err) => console.log(err.message));
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,13 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-
+const countriesMidd = require("../middlewares/countries.js");
+const activitiesMidd = require("../middlewares/activities.js");
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.use("/countries", countriesMidd);
 
 module.exports = router;
