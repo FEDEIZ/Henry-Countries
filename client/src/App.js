d353cf9c997213cc6bc2807981adf600e0cacfe9
fede_lastalitas <ing.finterlandi@gmail.com>
2022-09-09 11:57:13 -0300

    Creating a landing page for path '/'

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
-import CountryDetails from "./components/CountryDetails/CountryDetails";
-import Main from "./components/Main/Main";
+//import CountryDetails from "./components/CountryDetails/CountryDetails";
+import Main from "./components/Countries/Main";
+import {Landing} from "./components/Landing/Landing";
 
 function App() {
   return (
-    <React.Fragment>
-      {/* <NavBar /> */}
-      <Route exact path="/" component={Main} />
-      {/* <Route path="/favs" component={Favorites} /> */}
-      <Route path="/country/:id" component={CountryDetails} />
-    </React.Fragment>
+    <div className="App">
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/countries" component={Main}/>
+        {/* <Route path ="/activities"component={Activities} /> */}
+        {/* <Route exact path="/countries/:id" component={CountryDetails} /> */}
+    </div>
   );
 }
 
