a9c7cd982f43794e1178679629326a88de17a06b
fede_lastalitas <ing.finterlandi@gmail.com>
2022-09-13 11:28:12 -0300

    Created countries card component

--- client/src/components/Countries/Countries.js
+++ client/src/components/Countries/Countries.js
@@ -1,20 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import Country from "./Country"
 
 const Countries = () => {
   const countries = useSelector((state) => state.countries);
     return (
       <div>
-        {
-        countries.map((e) => (
-            <Link to={`/countries/${e.id}`}>
-                <div>
-                    <h2>{e.name}</h2>
-                </div>
-            </Link> 
-            ))
-        }
+        {countries.map(c => 
+          
+          <Link key={c.id} to={`/countries/${c.id}`}>
+            <Country name={c.name}  continent={c.continent} 
+              flag={c.flagImg} /> 
+          </Link>
+
+          )}
       </div>
     );
   }
