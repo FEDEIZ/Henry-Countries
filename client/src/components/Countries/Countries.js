c0d92d3e2732da031ac0a569098b5a8026318747
FEDEIZ <ing.finterlandi@gmail.com>
2022-09-16 18:02:20 -0300

    In Main we dispatch and in Countries we map states store

--- client/src/components/Countries/Countries.js
+++ client/src/components/Countries/Countries.js
@@ -1,23 +1,22 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import Country from "./Country"
+import Country from "./Country";
 
 const Countries = () => {
-  const countries = useSelector((state) => state.countries);
-    return (
-      <div>
-        {countries.map(c => 
-          
-          <Link key={c.id} to={`/countries/${c.id}`}>
-            <Country name={c.name}  continent={c.continent} 
-              flag={c.flagImg} /> 
-          </Link>
+  const countriesOrder = useSelector((state) => state.countriesOrder);
+  const countriesFilter = useSelector((state) => state.countriesFilter);
+  return countriesOrder.length ? (
+    <div>
+      {countriesOrder.map((c) => (
+        <Link key={c.id} to={`/countries/${c.id}`}>
+          <Country name={c.name} continent={c.continent} flag={c.flagImg} />
+        </Link>
+      ))}
+    </div>
+  ) : (
+    <h4>Loading countries</h4>
+  );
+};
 
-          )}
-      </div>
-    );
-  }
-
- 
 export default Countries;
--- client/src/components/Countries/Main.js
+++ client/src/components/Countries/Main.js
@@ -1,22 +1,32 @@
 import Countries from "../Countries/Countries.js";
 import React, { useEffect } from "react";
-import { getCountries } from "../../actions";
-import { useDispatch } from "react-redux";
-//import Nav from "../Nav/Nav.jsx";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  getCountries,
+  orderByAlphaAsc,
+  orderByAlphaDesc,
+  getCountriesActivities,
+  orderByPopAsc,
+  orderByPopDesc,
+  filterByActivities,
+  filterByContinents,
+} from "./../../actions";
 export function Main() {
+  const countries = useSelector((state) => state.countries);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getCountries());
+  useEffect(async () => {
+    if (!countries.length) {
+      await dispatch(getCountries());
+      await dispatch(getCountriesActivities());
+      dispatch(orderByAlphaAsc());
+    }
   }, [dispatch]);
 
-
   return (
-    
-      <div>
-        <Countries />
-      </div>
-    
+    <div>
+      <Countries />
+    </div>
   );
 }
 
