2d8f8aa84b8c941c4f6ec0fdebf25da5946bdf28
FEDEIZ <ing.finterlandi@gmail.com>
2022-09-15 21:18:11 -0300

    Erasing comments

--- api/src/db.js
+++ api/src/db.js
@@ -51,28 +51,6 @@ Activity.belongsToMany(Country, {
   timestamps: false,
 });
 
-// axios
-//   .get(`${API_URL}/all`)
-//   .then((countries) => {
-//     return countries.data;
-//   })
-//   .then((countries) => {
-//     countries.forEach(async (c) => {
-//       await Country.upsert({
-//         id: c.cca3,
-//         name: c.name.common,
-//         flagImg: c.flags[0],
-//         continent: c.region,
-//         capital: c.capital ? c.capital[0] : "NONE",
-//         subregion: c.subregion,
-//         area: c.area,
-//         population: c.population,
-//       });
-//     });
-//   })
-//   .then(() => console.log("Successfully loaded from api"))
-//   .catch((err) => console.log(err.message));
-
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
