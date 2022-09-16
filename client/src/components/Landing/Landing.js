939354813e54edfef95852081c61c784c02dd53f
FEDEIZ <ing.finterlandi@gmail.com>
2022-09-16 18:01:25 -0300

    Loading store when we stand in /

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-//import style from "./landing.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getCountries,
+  getCountriesActivities,
+  orderByAlphaAsc,
+} from "../../actions";
 
 export function Landing() {
+  const countries = useSelector((state) => state.countries);
+  const dispatch = useDispatch();
+
+  const loadStore = async () => {
+    await dispatch(getCountries());
+    await dispatch(getCountriesActivities());
+    dispatch(orderByAlphaAsc());
+  };
+
+  if (!countries.length) loadStore(); // initial values store
+
   return (
     <div>
       <div>
         <h1>Discover the world</h1>
         <div>
           <button>
-            <Link to="/countries">
-              Home
-            </Link>
+            <Link to="/countries">Home</Link>
           </button>
         </div>
       </div>
