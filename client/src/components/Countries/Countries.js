4f28154f7a1edd9aa2c83c8522e982284ea3f02b
FEDEIZ <ing.finterlandi@gmail.com>
2022-09-18 22:33:37 -0300

    Showing message if no country found. Component more atomic

--- client/src/components/Countries/Countries.js
+++ client/src/components/Countries/Countries.js
@@ -3,19 +3,23 @@ import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import Country from "./Country";
 
-const Countries = () => {
-  const countriesOrder = useSelector((state) => state.countriesOrder);
-  const countriesFilter = useSelector((state) => state.countriesFilter);
-  return countriesOrder.length ? (
-    <div>
-      {countriesOrder.map((c) => (
-        <Link key={c.id} to={`/countries/${c.id}`}>
-          <Country name={c.name} continent={c.continent} flag={c.flagImg} />
-        </Link>
-      ))}
-    </div>
+const Countries = ({ countries }) => {
+  useSelector((state) => state.countriesResultsFlag);
+
+  return Array.isArray(countries) ? (
+    countries.length ? (
+      <div>
+        {countries.map((c) => (
+          <Link key={c.id} to={`/countries/${c.id}`}>
+            <Country name={c.name} continent={c.continent} flag={c.flagImg} />
+          </Link>
+        ))}
+      </div>
+    ) : (
+      <h4>Loading countries</h4>
+    )
   ) : (
-    <h4>Loading countries</h4>
+    <h4>No country found</h4>
   );
 };
 
