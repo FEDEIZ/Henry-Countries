6b7765e82ccbe541bcaca1bb303ad571af33c745
fede_lastalitas <ing.finterlandi@gmail.com>
2022-09-20 12:13:52 -0300

    Order by selected list. Filter continents by checkbox. Filter not ready yet

--- client/src/components/Countries/Nav.js
+++ client/src/components/Countries/Nav.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  getCountries,
   orderByAlphaAsc,
   orderByAlphaDesc,
   getCountriesActivities,
@@ -22,7 +21,7 @@ export function Nav(){
     const order = useSelector((state) => state.order);
     const countriesResults = useSelector((state) => state.countriesResults);
 
-    const handleChange = (e) => {
+    const handleInputChange = (e) => {
         dispatch(searchByName(e.target.value));
         if(countriesResults.length)
         switch(order){
@@ -40,13 +39,48 @@ export function Nav(){
           return;
         }
       };
+
+    const handleOrderChange = (e) => {
+        switch (e.target.value){
+            case ALPHA_ASC:
+                if(order !== ALPHA_ASC) dispatch(orderByAlphaAsc());
+                return;
+            case ALPHA_DESC:
+                if(order !== ALPHA_DESC) dispatch(orderByAlphaDesc());
+                return;
+            case POP_ASC:
+                if(order !== POP_ASC) dispatch(orderByPopAsc());
+                return;
+            case POP_DESC:
+                if(order !== POP_DESC) dispatch(orderByPopDesc());
+                return;
+            default:
+                return;
+        }
+
+    }
     return (
         <nav>
-          <button onClick={() => dispatch(orderByAlphaAsc())}>ASC</button>
-          <button onClick={() => dispatch(orderByAlphaDesc())}>DESC</button>
-          <button onClick={() => dispatch(orderByPopAsc())}>POP_ASC</button>
-          <button onClick={() => dispatch(orderByPopDesc())}>POP_DESC</button>
-          <input type="text" placeholder="SearchByName" onChange={handleChange} />
+          <label>Order: </label>
+          <select name='order' onChange={handleOrderChange}>
+            <option value={ALPHA_ASC}>Alphabetic Ascending</option>
+            <option value={ALPHA_DESC}>Alphabetic Descending</option>
+            <option value={POP_ASC}>Population Ascending</option>
+            <option value={POP_DESC}>Population Descending</option>
+          </select>
+          <div> Filter By Continents: 
+            <input type="checkbox" name="EUROPE"/>
+            <label>Europe</label>
+            <input type="checkbox" name="AMERICAS"/>
+            <label>Americas</label> 
+            <input type="checkbox" name="ASIA"/>
+            <label>Asia</label> 
+            <input type="checkbox" name="AFRICA"/>
+            <label>Africa</label>
+            <input type="checkbox" name="OTHERS"/>
+            <label>Others</label>   
+          </div>
+          <input type="text" placeholder="SearchByName" onChange={handleInputChange} />
         </nav>
       );
 }
