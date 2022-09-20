8a562ad1d5ad22ff6f3cf4af519ade2ef0785d17
fede_lastalitas <ing.finterlandi@gmail.com>
2022-09-20 11:22:55 -0300

    Cleaner code

--- client/src/components/Countries/Countries.js
+++ client/src/components/Countries/Countries.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector} from "react-redux";
 import { Link } from "react-router-dom";
 import Country from "./Country";
 
 const Countries = ({ countries }) => {
-  //useSelector((state) => state.countriesResultsFlag);
   useSelector((state) => state.order);
   useSelector((state) => state.countriesResults.length);
 
