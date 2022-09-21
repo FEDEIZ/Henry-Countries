779a56cf987b85d879b7f14beb1a9519230938c0
fede_lastalitas <ing.finterlandi@gmail.com>
2022-09-21 09:39:21 -0300

    Paging seem to be complete

--- client/src/components/Countries/Countries.js
+++ client/src/components/Countries/Countries.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Country from "./Country";
@@ -6,10 +6,44 @@ import Country from "./Country";
 const Countries = ({ countries }) => {
   //useSelector((state) => state.countriesResults);
   //useSelector((state) => state.order);
+  const [countriesPerPage, setCountriesPerPage] = useState([]);
+  const [actualPage, setActualPage] = useState(0);
+  var totalPages = Math.round(countries.length/10) -1;
+
+
+  useEffect(()=>{
+    totalPages = Math.round(countries.length/10) -1;
+    if(actualPage === 0) setCountriesPerPage([].concat(countries.slice(0,9)))
+    else setCountriesPerPage([].concat(countries.slice(actualPage*10,actualPage*10+10)))
+  },[countries,actualPage]);
+
+  
+  const handlePageChange =(e) =>{
+    switch(e.target.value) {
+      case 'FIRST':
+        setActualPage(0);
+        return;
+      case 'LAST':
+        setActualPage(actualPage-1);
+        return;
+      case 'NEXT':
+        setActualPage(actualPage+1);
+        return;
+      case 'FINAL':
+        setActualPage(totalPages)
+        return;
+    }
+  }
 
   return countries.length ? (
     <div>
-      {countries.map((c) => (
+      <div>
+        <button onClick={handlePageChange} value={'FIRST'} disabled={!actualPage}>{'<<'}</button>
+        <button onClick={handlePageChange} value={'LAST'} disabled={actualPage - 1 < 0}>{'<'}</button>
+        <button onClick={handlePageChange} value={'NEXT'} disabled={actualPage + 1 > totalPages}>{'>'}</button>
+        <button onClick={handlePageChange} value={'FINAL'} disabled={actualPage === totalPages}>{'>>'}</button>
+      </div>
+      {countriesPerPage.map((c) => (
         <Link key={c.id} to={`/countries/${c.id}`}>
           <Country name={c.name} continent={c.continent} flag={c.flagImg} />
         </Link>
